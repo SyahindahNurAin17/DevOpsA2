d8aabb5b324c4da89b1b57336c3ae1a2f3111fd1
syahindah nur'ain binti awg md sharol shaheezam <23FTT1873@student.pb.edu.bn>
2025-09-08 10:10:47 +0800

    Update app-logo.blade.php

--- resources/views/components/app-logo.blade.php
+++ resources/views/components/app-logo.blade.php
@@ -1,4 +1,5 @@
 <div class="flex aspect-square size-8 items-center justify-center rounded-md bg-accent-content text-accent-foreground">
+    <img src="{{ asset('images/logo.png') }}" alt="Logo" {{ $attributes }}>
     <x-app-logo-icon class="size-5 fill-current text-white dark:text-black" />
 </div>
 <div class="ms-1 grid flex-1 text-start text-sm">
