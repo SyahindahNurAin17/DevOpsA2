11ebee57c15ed7a06a75d885c3fdce28263ca6fc
syahindah nur'ain binti awg md sharol shaheezam <23FTT1873@student.pb.edu.bn>
2025-09-08 10:09:49 +0800

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -53,6 +53,7 @@
         <div class="flex items-center justify-center w-full transition-opacity opacity-100 duration-750 lg:grow starting:opacity-0">
             <main class="flex max-w-[335px] w-full flex-col-reverse lg:max-w-4xl lg:flex-row">
                 <div class="text-[13px] leading-[20px] flex-1 p-6 pb-12 lg:p-20 bg-white dark:bg-[#161615] dark:text-[#EDEDEC] shadow-[inset_0px_0px_0px_1px_rgba(26,26,0,0.16)] dark:shadow-[inset_0px_0px_0px_1px_#fffaed2d] rounded-es-lg rounded-ee-lg lg:rounded-ss-lg lg:rounded-ee-none">
+                    <img src="{{ asset('images/logo.png') }}" alt="Logo" {{ $attributes }}>
                 <div class="mb-4">
                     <a href="{{ route('home') }}" class="inline-flex items-center gap-2 font-medium" wire:navigate>
                         <span class="flex h-9 w-9 items-center justify-center rounded-md">
